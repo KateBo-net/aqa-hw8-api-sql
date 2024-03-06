7e7a343f87727787afdca81b1a58283d69036586
Maxim Bodrak <m.bodrak@eastwind.ru>
2024-03-06 21:50:12 +0300

    add assertions

--- src/test/java/data/DataHelper.java
+++ src/test/java/data/DataHelper.java
@@ -1,8 +1,14 @@
 package data;
 
+import java.util.Random;
+
 public class DataHelper {
 
     public static String generateCardNumber(String number) {
         return "5559 0000 0000" + number.substring(number.length() - 4);
+    }
+
+    public static int generateAmount(int balance){
+        return new Random().nextInt(balance)+1;
     }
 }
--- src/test/java/test/TransferTest.java
+++ src/test/java/test/TransferTest.java
@@ -8,6 +8,8 @@ import java.util.List;
 import static api.RequestHelper.*;
 import static data.DataHelper.*;
 import static data.SQLHelper.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TransferTest {
 
@@ -17,16 +19,24 @@ public class TransferTest {
     }
 
     @Test
-    public void shouldTransferMoney(){
+    public void shouldTransferMoney() {
         AuthInfo user = new AuthInfo("vasya", "qwerty123");
         postAuth(user);
         VerifyInfo verifyInfo = new VerifyInfo(user.getLogin(), getVerificationCode());
         String token = postVerify(verifyInfo);
         List<CardInfo> cardList = getCards(token);
-        int amount = cardList.get(0).getBalance();
+
+        int balanceFrom = cardList.get(0).getBalance();
+        int balanceTo = cardList.get(1).getBalance();
+        int amount = generateAmount(balanceFrom);
         String from = generateCardNumber(cardList.get(0).getNumber());
         String to = generateCardNumber(cardList.get(1).getNumber());
         TransferInfo transfer = new TransferInfo(from, to, amount);
         postTransfer(token, transfer);
+        List<CardInfo> cardListResult = getCards(token);
+        assertAll(
+                () -> assertEquals(balanceFrom - amount, cardListResult.get(0).getBalance()),
+                () -> assertEquals(balanceTo + amount, cardListResult.get(1).getBalance())
+        );
     }
 }
