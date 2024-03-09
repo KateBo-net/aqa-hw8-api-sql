4abca53460b25f2464181bc8307ef8836b69835c
KateBo <bodrakea@gmail.com>
2024-03-09 16:49:54 +0300

    fix generateCardNumber

--- src/test/java/data/DataHelper.java
+++ src/test/java/data/DataHelper.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class DataHelper {
 
     public static String generateCardNumber(String number) {
-        return "5559 0000 0000" + number.substring(number.length() - 4);
+        return "5559 0000 0000 " + number.substring(number.length() - 4);
     }
 
     public static int generateAmount(int balance){
